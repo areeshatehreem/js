b693f7154c08f63945e53a3c0f2c01b2fd377c45
areeshatehreem <50449369+areeshatehreem@users.noreply.github.com>
2019-07-16 17:58:26 +0500

    wip

---
+++ script.js
@@ -0,0 +1,62 @@
+
+//fuction declaration/ defination
+//step 1
+
+function addTwoNumber(num1, num2) {
+    return num1 + num2;
+}
+
+//step 2 is also called "call" in the language of programmer
+
+addTwoNumber(3, 6);
+
+
+function square(numb1) {
+    return numb1 ** 2
+}
+square(4);
+
+
+function subtractTwoNumber(nu1, nu2) {
+    return nu1 - nu2;
+}
+
+//step 2 is also called "call" in the language of programmer
+
+subtractTwoNumber(8, 4)
+
+function cube(numb1) {
+    return numb1 ** 3
+}
+cube(4);
+
+
+function humanName(name){
+    return  "hello"  +   name;
+    
+}
+humanName("areesha");
+
+
+
+function divideTwoNumber(nu1, nu2) {
+    return nu1 / nu2;
+}
+
+
+
+divideTwoNumber(16,4);
+
+//let humanName = muskan; 
+
+
+//function  humanNames(muskan){
+  //  return {
+    //    name: "muskan",
+      //  age: 14,
+        //phoneNumber: 345565756,
+
+    //}
+//}
+//humanNames("muskan");
+
